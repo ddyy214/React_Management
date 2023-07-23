15d8b28554de77ec873fec41b1b79a708f759f49
ddyy <wntnftk79@gamil.com>
2023-07-24 01:45:55 +0900

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,48 @@
 import './App.css';
-import { Component } from 'react';
-import Customer from './components/Customer';
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
-//보내고자 하는 데이터를 명시해주기
-//customer 변수를 만들기
-const customers = [
-  {
-  'id':1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '유다연',
-  'birthday' : '96122',
-  'gender' : '여자',
-  'job' : '대학생'
-},
-{
-  'id':2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '나신입',
-  'birthday' : '940702',
-  'gender' : '남자',
-  'job' : '직장인'
-},
-{
-  'id':3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '홍길동',
-  'birthday' : '031111',
-  'gender' : '남자',
-  'job' : '경찰'
-}]
+export default function App() {
 
-//화면 body를 담당하는 부분
-//Component란 말 그대로 웹 문서에서 어떠한 내용을 보여주기 위한 기본적인 단위라고 생각하자
-class App extends Component {
-  render() {
-    return (
-    <div>
-      {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gende={c.gender} job={c.job}/> ); })  }
-    </div>
-    );
-  }
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+           marginTop: 8,
+           display: 'flex',
+           flexDirection: 'column',
+           alignItems: 'center',
+        }}
+        >
+       <Avatar sx={{ m: 2, bgcolor: "gray.main"}}>
+        <LockOutlinedIcon />
+        </Avatar>
+       <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+      <TextField margin="normal" label="Email Address" required fullWidth name="email" autoComplete="email"/>
+      <TextField label="Password" type="password" required fullWidth name="password" />
+      <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me"/>
+      <Button type="submit" fullWidth variant="contained" sx={{mt:3, mb:2 }}>Sign in</Button>
+      <Grid container>
+        <Grid item xs>
+           <Link>Forgot Password?</Link>
+        </Grid>
+        <Grid item>
+           <Link>Sign up</Link>
+        </Grid>
+     </Grid>
+    </Box>
+    </Container>
+  )
 }
-
-export default App;
